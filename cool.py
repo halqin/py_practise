bd4d360f7da115100cafc4e2c253d45aa555edbf
Hao Qin <hao.qin@ugent.be>
2019-11-09 12:17:57 +0100

    billion

---
+++ cool.py
@@ -0,0 +1,18 @@
+def serialNumber():
+
+	'''
+	>>> serialNumber(834783)
+	'00834783'
+	>>> serialNumber('47839')
+	'00047839'
+	>>> serialNumber(834783244839184)
+	'834783244839184'
+	>>> serialNumber('4783926132432*')
+	Traceback (most recent call last):
+	AssertionError: invalid serial number
+
+	'''
+
+	return 
+
+
